66196eddf175afe3821e5a8880f17ae161a1c70b
Ivan Molto <ivanmolto@gmail.com>
2022-09-20 16:19:31 +0200

    chore: Add two boxes in the scene

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Box from "./Box";
+import { Canvas } from "@react-three/fiber";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <Box position={[-1.2, 0, 0]}/>
+      <Box position={[1.2, 0, 0]}/>
+    </Canvas>
   );
 }
 
